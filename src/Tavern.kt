1837d50fdaf8c7e60a368dc61aa93404390b3fb4
Julian Cabrera <julian.cabreras@gmail.com>
2019-12-30 16:08:05 -0600

    Chapter 8 numbers

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,9 +1,27 @@
 const val TAVERN_NAME ="Taernyl's Folly"
+var playerGold=10
+var playerSilver =10
+
 
 fun main(args: Array<String>) {
     placeOrder("shandy,Dragon's Breath,5.91")
+
 }
 
+fun performPurchase(price: Double){
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver/100.0)
+    print("Total purse : $totalPurse")
+
+    println("purchasing item for $$price")
+
+    val remainingBalance = totalPurse- price
+    println("remaining balance  ${"%.2f".format(remainingBalance)}")
+}
+
+fun displayBalance() {
+    println("Players purse balance: Gold $playerGold, Silver $playerSilver ")
+}
 
 private fun placeOrder(menuData: String) {
     var indexofApostrophe = TAVERN_NAME.indexOf('\'')
@@ -14,6 +32,8 @@ private fun placeOrder(menuData: String) {
     val(type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
+
+    performPurchase(price.toDouble())
 
     val phrase = if (name == "Dragon's Breath") {
         "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
